d93366d75cd12aaf9b5720bf2ab089914abb1f97
Randy Reiza <randyreiza14@gmail.com>
2022-06-18 18:26:35 +0700

    feat(home): retrieve the arguments passed from home fragment at detail fragment

--- app/src/main/java/com/example/mrcook/ui/detail/DetailFragment.java
+++ app/src/main/java/com/example/mrcook/ui/detail/DetailFragment.java
@@ -1,27 +1,34 @@
 package com.example.mrcook.ui.detail;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.example.mrcook.R;
+import com.example.mrcook.databinding.FragmentDetailBinding;
+import com.example.mrcook.entity.Food;
 
 public class DetailFragment extends Fragment {
 
+    private FragmentDetailBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        binding = FragmentDetailBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Food food = DetailFragmentArgs.fromBundle(getArguments()).getFood();
+
+        binding.tv.setText(food.getTitle() + "\n" + food.getThumb() + "\n" + food.getDificulty() + "\n" + food.getPortion() + "\n" + food.getKey() + "\n");
     }
 }
