dd6920f9f9b92d4dbb899ef518babef124f09303
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2022-06-22 18:10:42 +0700

    feat(login): update LoginFragment

--- app/src/main/java/com/example/mrcook/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/mrcook/ui/login/LoginFragment.java
@@ -1,66 +1,90 @@
 package com.example.mrcook.ui.login;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mrcook.R;
+import com.example.mrcook.databinding.FragmentLoginBinding;
+import com.example.mrcook.helper.ViewModelFactory;
+import com.example.mrcook.repository.LoginRepository;
+import com.example.mrcook.restapi.user.responses.ResponseUser;
+import com.example.mrcook.ui.MainActivity;
+import com.google.gson.Gson;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FragmentLoginBinding binding;
+    private EditText inputUsername;
+    private EditText inputPassword;
+    private Button btnSignIn;
+    private Button btnSignUp;
+    private LoginViewModel viewModel;
+    private LoginRepository loginRepository;
+    String username;
+    String password;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        btnSignIn = view.findViewById(R.id.btn_signIn);
+        btnSignUp = view.findViewById(R.id.btn_signUp);
+        inputUsername= view.findViewById(R.id.inputUsername);
+        inputPassword = view.findViewById(R.id.inputPassword);
+
+        loginRepository = new LoginRepository(this.getActivity().getApplication());
+        viewModel = new LoginViewModel(loginRepository);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                username = inputUsername.getText().toString();
+//                password = inputPassword.getText().toString();
+                viewModel.userLogin();
+                if(viewModel.status == true) {
+                Toast.makeText(getContext(), "Login Success", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        );
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //for sign up process
+            }
+        });
+
+    }
+
+    private LoginViewModel obtainViewModel(FragmentActivity activity){
+        ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
+        return new ViewModelProvider(activity, (ViewModelProvider.Factory) factory).get(LoginViewModel.class);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
