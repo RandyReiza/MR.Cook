61c2d0a6ec716c1200a40e1d05341a70121d3d85
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2022-06-22 18:10:10 +0700

    feat(login): add LoginRepository

---
+++ app/src/main/java/com/example/mrcook/repository/LoginRepository.java
@@ -0,0 +1,24 @@
+package com.example.mrcook.repository;
+
+import android.app.Application;
+
+import com.example.mrcook.BuildConfig;
+import com.example.mrcook.restapi.foodrecipe.ApiConfig;
+import com.example.mrcook.restapi.user.responses.ResponseUser;
+
+import retrofit2.Call;
+
+public class LoginRepository {
+
+    private Application application;
+    static final String BASE_URL = BuildConfig.BASE_URL;
+
+
+    public LoginRepository(Application application) {
+        this.application = application;
+    }
+
+    public Call<ResponseUser> userLogin(){
+        return new ApiConfig(BASE_URL).getApiService().userLogin();
+    }
+}
