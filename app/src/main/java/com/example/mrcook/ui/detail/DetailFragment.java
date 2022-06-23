d8f325b6de9c7c95a46dcd4301e9087fb0df5139
Randy Reiza <randyreiza14@gmail.com>
2022-06-23 21:45:12 +0700

    feat(detail): create and implement detail fragment including its viewModel

--- app/src/main/java/com/example/mrcook/ui/detail/DetailFragment.java
+++ app/src/main/java/com/example/mrcook/ui/detail/DetailFragment.java
@@ -1,6 +1,7 @@
 package com.example.mrcook.ui.detail;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +9,27 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.mrcook.R;
+import com.example.mrcook.database.FoodData;
 import com.example.mrcook.databinding.FragmentDetailBinding;
 import com.example.mrcook.entity.Food;
+import com.example.mrcook.helper.Helper;
+import com.example.mrcook.helper.ViewModelFactory;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class DetailFragment extends Fragment {
 
     private FragmentDetailBinding binding;
+    private DetailViewModel viewModel;
+    private Food food;
+
+    private FoodData foodData = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -27,8 +42,97 @@ public class DetailFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Food food = DetailFragmentArgs.fromBundle(getArguments()).getFood();
+        food = DetailFragmentArgs.fromBundle(getArguments()).getFood();
 
-        binding.tv.setText(food.getTitle() + "\n" + food.getThumb() + "\n" + food.getDificulty() + "\n" + food.getPortion() + "\n" + food.getKey() + "\n");
+        viewModel = obtainViewModel(requireActivity());
+
+        observeAllValues();
+
+        binding.buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requireActivity().onBackPressed();
+            }
+        });
+
+        binding.buttonFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeFavorite(food);
+            }
+        });
+        
+        Glide.with(this)
+                .load(food.getThumb())
+                .apply(RequestOptions.bitmapTransform(new BlurTransformation(25, 3)))
+                .into(binding.imgCover);
+        Glide.with(this)
+                .load(food.getThumb())
+                .apply(new RequestOptions().override(340, 170))
+                .into(binding.imgView);
+
+        // title
+        binding.title.setText(food.getTitle().replace("Resep ", ""));
+
+        // dificulty
+        String dificulty;
+        if (food.getDificulty().equals("Mudah")) {
+            dificulty = "Easy";
+        } else if (food.getDificulty().equals("Cukup Rumit")) {
+            dificulty = "Medium";
+        } else {
+            dificulty = "Hard";
+        }
+        binding.dificulty.setText(dificulty);
+
+        // portion
+        binding.portion.setText(food.getPortion().split(" ")[0] + " Portion");
+
+        // times
+        binding.times.setText(food.getTimes());
+
+        binding.description.setText(food.getDesc());
+
+        String ingredient = TextUtils.join(" \n", food.getIngredient());
+        binding.ingredient.setText(ingredient);
+
+        String step = TextUtils.join(" \n", food.getStep());
+        binding.step.setText(step);
     }
+
+    private DetailViewModel obtainViewModel(FragmentActivity activity) {
+        ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
+        return new ViewModelProvider(activity, (ViewModelProvider.Factory) factory).get(DetailViewModel.class);
+    }
+
+    private void observeAllValues() {
+        viewModel.findFoodByKey(food.getKey()).observe(getViewLifecycleOwner(), it -> {
+            foodData = it;
+            if (foodData != null) binding.buttonFavorite.setImageResource(R.drawable.ic_baseline_favorite_24); else binding.buttonFavorite.setImageResource(R.drawable.ic_baseline_favorite_border_24);
+        });
+        viewModel.isLoading.observe(getViewLifecycleOwner(), it -> {
+            Helper.showView(binding.progressBar, it);
+        });
+    }
+
+    private void changeFavorite(Food food) {
+        if (foodData != null) {
+            viewModel.deleteFood(foodData);
+            setFavoriteState(false, food.getTitle());
+        } else {
+            viewModel.insertFood(food);
+            setFavoriteState(true, food.getTitle());
+        }
+    }
+
+    private void setFavoriteState(Boolean state, String title) {
+        if (state) {
+            binding.buttonFavorite.setImageResource(R.drawable.ic_baseline_favorite_24);
+            Helper.showToast(requireContext(), getResources().getString(R.string.add_to_favorite, title));
+        } else {
+            binding.buttonFavorite.setImageResource(R.drawable.ic_baseline_favorite_border_24);
+            Helper.showToast(requireContext(), getResources().getString(R.string.remove_from_favorite, title));
+        }
+    }
+
 }
