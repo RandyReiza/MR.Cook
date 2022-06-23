aca25add86214c23f3e2bec0bbfea01f13018c81
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2022-06-23 14:42:25 +0700

    feat(login): update LoginViewModel (add try catch onResponse)

--- app/src/main/java/com/example/mrcook/ui/login/LoginViewModel.java
+++ app/src/main/java/com/example/mrcook/ui/login/LoginViewModel.java
@@ -18,7 +18,7 @@ import retrofit2.Response;
 public class LoginViewModel extends ViewModel {
 
     private final LoginRepository loginRepository;
-    boolean status = false ;
+    boolean status = false;
     public LoginViewModel(LoginRepository loginRepository) {
         this.loginRepository = loginRepository;
     }
@@ -30,16 +30,20 @@ public class LoginViewModel extends ViewModel {
         client.enqueue(new Callback<ResponseUser>() {
             @Override
             public void onResponse(Call<ResponseUser> call, Response<ResponseUser> response) {
-                if(response.isSuccessful()){
-//                    loginRepository.userLogin();
-                    status = true;
-             }
+                try {
+                    ResponseUser responseBody = response.body();
+                    if(response.isSuccessful() && responseBody != null){
+                        status = true;
+                    }
+                }
+                catch (Exception e) {
+                    Log.e(TAG, e.getMessage());
+                }
             }
 
             @Override
             public void onFailure(Call<ResponseUser> call, Throwable t) {
                 Log.e(TAG, t.getMessage());
-//                Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
             }
         });
     }
