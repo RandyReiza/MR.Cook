891bcfbb67d2e407000fc184c80ffc088450802c
Randy Reiza <randyreiza14@gmail.com>
2022-06-24 23:27:07 +0700

    refactor(profile): clean code

--- app/src/main/java/com/example/mrcook/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mrcook/ui/profile/ProfileFragment.java
@@ -3,26 +3,18 @@ package com.example.mrcook.ui.profile;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.example.mrcook.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
 import com.example.mrcook.databinding.FragmentProfileBinding;
-import com.example.mrcook.helper.ViewModelFactory;
 import com.example.mrcook.session.SessionManagerUtil;
-import com.example.mrcook.ui.MainActivity;
 import com.example.mrcook.ui.login.LoginActivity;
 import com.example.mrcook.ui.login.LoginViewModel;
 
@@ -48,18 +40,10 @@ public class ProfileFragment extends Fragment {
             binding.tvEmail.setText(sessionManagerUtil.getEmail());
             binding.tvUsername.setText(sessionManagerUtil.getUsername());
 
-//        viewModel = obtainViewModel(requireActivity());
-//        viewModel.user.observe(getViewLifecycleOwner(), it -> {
-//            binding.tvFullname.setText(it.getFullName().toString());
-//            binding.tvEmail.setText(it.getEmail().toString());
-//            binding.tvUsername.setText(it.getUsername().toString());
-//
-//        });
         //if button back onclick
         binding.arrowBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "Back to Home", Toast.LENGTH_LONG).show();
                 navigateToHome(v);
             }
         });
@@ -79,10 +63,6 @@ public class ProfileFragment extends Fragment {
                 startActivity(intent);
             }
         });
-    }
-    private LoginViewModel obtainViewModel(FragmentActivity activity){
-        ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
-        return new ViewModelProvider(activity, (ViewModelProvider.Factory) factory).get(LoginViewModel.class);
     }
 
     private void navigateToHome(View view){
